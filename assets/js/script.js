004d6c1f6cbe46f4d70256f857f78d2fa926898d
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-05 15:18:41 -0300

    Canectando o JavaScript com o HTML e estilizando com o CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello Wolrd");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
