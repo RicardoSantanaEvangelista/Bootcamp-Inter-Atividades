6c0c38b81addcfc6b4b1334a45921127a302c657
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-05 14:06:59 -0300

    Funções com JavaScript

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Bootcamp-Inter-Atividades
-Nesse repositório será adicionado as atividades do Bootcamp Inter da DIO
+Nesse repositório será adicionado as atividades do Bootcamp Inter da DIO, relacionadas a linguagem de programação JS (JavaScript) !
---
+++ atividade.js
@@ -0,0 +1,22 @@
+
+// Funções
+
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }else{
+            console.log(`${array[i]} - > Não é par !`);
+        }
+    }
+    console.log("Os números pares são: ",evenNums);
+}
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+returnEvenValues(array);
+
+
+
+
