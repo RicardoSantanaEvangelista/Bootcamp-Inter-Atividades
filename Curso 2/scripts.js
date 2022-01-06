e3cda909160366645a5eaf2969329a60e5669abd
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-06 14:16:44 -0300

    Mais atividades do Bootcamp Inter da Dio, em JavaScript

---
+++ Curso 2/scripts.js
@@ -0,0 +1,92 @@
+alert("Tudo certo por enquanto !");
+
+
+/*// Tipos Primitivos
+
+// Boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+// Number
+var numb = 1;
+console.log(typeof(numb));
+
+
+//String
+var nome = 'Ricardo';
+console.log(typeof(nome));
+
+// Variaveis
+
+var variavel; // Escopo local e global e se não for atribuida nenhum valor, será tratada como null/undefined
+let variavel; // Espoco local e se não for atribuida nenhum valor será tratada como null/undefined
+const variavel = 20; // Não pode ser alterada e tem que ter um valor fixo e tem que ter um valor inicial
+ */
+
+// Estrutura Condicionais
+
+/*var jogador1 = 1;
+var jogador2 = 1;
+var placar;*/
+
+/*if (jogador1 != -1){
+    if(jogador1 > 0){
+        console.log("Jogador 1 marcou ponto");
+    }else if(jogardor2 > 0){
+        console.log("Jogador 2 marcou ponto");
+    }else{
+        console.log("Ninguém marcou ponto");
+    }
+}else{
+    console.log("Jogada inválida !");
+}*/
+
+/*switch (placar) {
+    case placar = jogador1 > jogador2:
+    console.log("Jogador 1 ganhou !");
+    break;
+
+    case placar = jogador2 > jogador1:
+        console.log("jogador 2 ganhou !");
+        break;
+    default:
+        console.log("Ninguém ganhou !");
+}*/
+
+// Estrutura de repetição
+
+let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+
+// FOT
+
+/*for(let indice = 0; indice < array.length; indice ++){
+    console.log(indice);
+}**/
+
+// FOR IN
+
+/*for(let i in array){
+    console.log(i);
+}*/
+
+// FOR OF
+
+/*for(i of array){
+    console.log(i);
+}*/
+
+
+// WHILE 
+var a = 0;
+
+/*while (a < 10){
+    a++;
+    console.log(a);
+}*/
+
+// DO WHILE
+
+do {
+    a++;
+    console.log(a);
+}while(a < 10);
