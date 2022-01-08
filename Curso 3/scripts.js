a413589682b384aa12e621282f6e9a94f9630409
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-08 15:40:44 -0300

    Adicionando mais uma atividade do Bootcamp da DIO

---
+++ Curso 3/scripts.js
@@ -0,0 +1,63 @@
+// Operadores 
+
+// ATRIBUIÇÃO 
+
+// = Ex: x = y 
+
+// +=  Ex: x +=y / x = x + y
+
+// *= Ex: x *= y / x = x * y
+
+// /= Ex: x/=y / x = x / y
+
+// %= Ex: x %=y / x = x% y
+
+
+// Operadores Aritiméticos 
+
+/*Adição: +
+
+Subtração: -
+
+Multiplicação : * 
+Exponencial: **
+
+Divisão: /
+
+Módulo: %
+
+Incrementar: ++
+
+Decrementar: --*/
+
+
+/*Operadores de Comparação 
+
+Igual a : ==
+
+Mesmo valor e mesmo tipo: ===
+
+Diferente: !=
+
+Valor e tipos diferentes: !==
+
+Maior que : >
+
+Menos que : <
+
+Maior igual: >= 
+
+Menor igual: <=
+*/
+
+/*Operadores Lógicos 
+
+&& "é lógico"
+
+|| "ou lógico"
+
+! "Não lógico"*/
+
+/*Operador Condicilnal / IF ternario
+
+(condição) ? expressão1 : expressão2*/
