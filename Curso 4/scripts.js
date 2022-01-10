8efd68277622a4e58068fe7897c3ce07549667f3
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-10 16:13:07 -0300

    Curso 4 do Bootcamp da Dio

---
+++ Curso 4/scripts.js
@@ -0,0 +1,59 @@
+
+
+/*nomberOne = 1;
+
+console.log(numberOne + 2)
+
+var numberOne;*/
+
+
+/*----------------------------------------------------------------------------------------------*/
+
+/*var firstName = 'João';
+var lastName = "Santana";
+
+if(firstName === 'João'){
+    var firstName = 'Ricardo';
+    let lastName = 'Evangelista';
+}
+
+
+console.log(firstName, lastName);*/
+
+/*----------------------------------------------------------------------------------------------*/
+
+/*const  FIRST_NAME = "RICARDO";
+
+console.log(FIRST_NAME);*/
+
+/*ESTRURA DE DADOS*/
+
+
+let nome = "João Ricardo";
+
+let sobreNome = "De Santana Evangelista";
+
+let name = new String("João Ricardo de Santana Evangelista"); //Objeto
+
+console.log(name.length);
+
+console.log(nome, " ",sobreNome); // Formas de concatenar
+
+console.log(nome,"\n",sobreNome); // Quebrando a linha
+
+console.log(`${nome} ${sobreNome} = ${name}`); // Outra forma de concatenar
+
+/*métodos* */
+
+console.log(nome.split("")); // Seprando por letra
+
+console.log(nome.split(" ")); // Separando por palavra
+
+console.log(nome.includes("Ricardo")); // procurando por palavra
+
+console.log(nome.startsWith("J")) // Perguntando se a frase ou palavra começa com J
+
+console.log(nome.endsWith("O")) // pERGUNTANDO SE A FRASE TERMINA COM o
+
+console.log(nome.replace("J", "R")); // Mudando as letras de uma frase, podendo alterar qualquer parte dela
+
